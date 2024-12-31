38bbc11bdf8847930122d142a3ebbbf4ab56875a
yskszk63 <yskszk63@gmail.com>
2024-12-31 17:05:23 +0900

    Refactor

--- web/src/components/graphviz.tsx
+++ web/src/components/graphviz.tsx
@@ -1,5 +1,65 @@
 import { useCallback, useEffect, useState } from "react";
-import { LibGraphviz } from "../lib/libgraphviz";
+import { LibGraphviz } from "@/lib/libgraphviz";
+
+function useLib(): LibGraphviz | undefined {
+  const [lib, setLib] = useState<LibGraphviz | undefined>();
+
+  useEffect(() => {
+    const abort = new AbortController();
+    (async (signal) => {
+      const lib = await LibGraphviz.create("libwasinodot.wasm");
+      if (signal.aborted) {
+        return;
+      }
+      setLib(lib);
+    })(abort.signal);
+    return () => abort.abort();
+  }, []);
+
+  return lib;
+}
+
+type UseRenderedResult = {
+  image?: string | undefined;
+  error?: string | undefined;
+}
+
+function useRendered(lib: LibGraphviz | undefined, src: Uint8Array): UseRenderedResult {
+  const [blob, setBlob] = useState<Blob | null>(null);
+  const [stderr, setStderr] = useState<string[]>(() => []);
+  const [state, setState] = useState<UseRenderedResult>(() => ({}));
+
+  useEffect(() => {
+    if (typeof lib === "undefined") {
+      return;
+    }
+
+    const [blob, stderr] = lib.render(src);
+    setBlob(blob);
+    setStderr(stderr);
+  }, [lib, src]);
+
+  useEffect(() => {
+    if (blob === null) {
+      setState(({ image: _, ...prev }) => ({ ...prev }));
+      return;
+    }
+
+    const image = URL.createObjectURL(blob);
+    setState((prev) => ({ ...prev, image }));
+    return () => URL.revokeObjectURL(image);
+  }, [blob]);
+
+  useEffect(() => {
+    if (stderr.length < 1) {
+      setState(({ error: _, ...prev }) => ({ ...prev }));
+      return;
+    }
+    setState((prev) => ({ ...prev, error: stderr.join("") }));
+  }, [stderr]);
+
+  return state;
+}
 
 interface Props {
   src: Uint8Array;
@@ -8,36 +68,10 @@ interface Props {
 }
 
 function Graphviz({ src, onError, className }: Props) {
-  const [lib, setLib] = useState<LibGraphviz | null>(null);
-  const [image, setImage] = useState<string | null>(null);
-  const onErrorCallback = useCallback((msg: string | null) => {
-    if (onError) {
-      onError(msg);
-    }
-  }, [onError]);
+  const lib = useLib();
+  const { image, error } = useRendered(lib, src);
 
-  useEffect(() => {
-    LibGraphviz.create("libwasinodot.wasm").then(setLib);
-  }, []);
-
-  useEffect(() => {
-    if (!lib) {
-      return;
-    }
-
-    const [blob, stderr] = lib.render(src);
-    const image = blob ? URL.createObjectURL(blob) : null;
-    if (image) {
-      setImage(image);
-    }
-    onErrorCallback(stderr.length ? stderr.join("") : null);
-
-    return () => {
-      if (image) {
-        URL.revokeObjectURL(image);
-      }
-    };
-  }, [lib, src, onErrorCallback, setImage]);
+  useEffect(() => onError?.(error ?? null), [error, onError]);
 
   return (
     <div className={className}>
